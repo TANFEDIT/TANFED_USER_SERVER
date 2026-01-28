c949900dd108f360256c9e68860e90170c97b0dd
TANFEDIT <tanfeditteam@gmail.com>
2026-01-28 17:29:29 +0530

    support post api changed

--- src/main/java/com/tanfed/user/controller/SupportHandler.java
+++ src/main/java/com/tanfed/user/controller/SupportHandler.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +16,6 @@ import com.tanfed.user.service.SupportService;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/api/user/support")
@@ -25,12 +25,13 @@ public class SupportHandler {
 	private SupportService supportService;
 
 	@PostMapping("/saveissue")
-	public ResponseEntity<String> saveIssueHandler(@RequestBody String issue, String jwt) throws Exception {
+	public ResponseEntity<String> saveIssueHandler(@RequestBody String issue,
+			@RequestHeader("Authorization") String jwt) throws Exception {
 		return supportService.saveIssue(issue, jwt);
 	}
 
 	@GetMapping("fetchissuesbyempid")
-	public List<IssueData> fetchIssuesByEmpIdHandler(@RequestParam String jwt) throws Exception {
+	public List<IssueData> fetchIssuesByEmpIdHandler(@RequestHeader("Authorization") String jwt) throws Exception {
 		return supportService.fetchIssuesByEmpId(jwt);
 	}
 
@@ -40,7 +41,8 @@ public class SupportHandler {
 	}
 
 	@PutMapping("/updateissuestatus/{issueId}/{status}")
-	public ResponseEntity<String> updateIssueHandler(@PathVariable String issueId, String status) throws Exception {
+	public ResponseEntity<String> updateIssueHandler(@PathVariable String issueId, @PathVariable String status)
+			throws Exception {
 		return supportService.updateIssue(issueId, status);
 	}
 
